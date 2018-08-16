53607528112af82cc8c1e039a75b5362dd7dfd67
空调遥控器 <1610353233@qq.com>
2018-08-16 22:12:23 +0800

    最近心跳快

---
+++ src/leetCode/L_119_PascalIsTriangleII.java
@@ -0,0 +1,24 @@
+package leetCode;
+
+public class L_119_PascalIsTriangleII {
+
+	// Given a non-negative index k where k ≤ 33, return the kth index row of
+	// the Pascal's triangle.
+	//
+	// Note that the row index starts from 0.
+
+	public static void main(String[] args) {
+		int rows = 10;
+
+		for (int i = 0; i < rows; i++) {
+			int number = 1;
+			// 打印空格字符串
+			System.out.format("%" + (rows - i) * 2 + "s", "");
+			for (int j = 0; j <= i; j++) {
+				System.out.format("%4d", number);
+				number = number * (i - j) / (j + 1);
+			}
+			System.out.println();
+		}
+	}
+}
