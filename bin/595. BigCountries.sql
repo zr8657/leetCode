bb0455854bb4bddc1b68379f10f3aa9207357707
空调遥控器 <1610353233@qq.com>
2018-08-19 15:48:55 +0800

    每次做题都能够重新认识自己的菜鸡程度

---
+++ bin/595. BigCountries.sql
@@ -0,0 +1,34 @@
+There is a table World
+
++-----------------+------------+------------+--------------+---------------+
+| name            | continent  | area       | population   | gdp           |
++-----------------+------------+------------+--------------+---------------+
+| Afghanistan     | Asia       | 652230     | 25500100     | 20343000      |
+| Albania         | Europe     | 28748      | 2831741      | 12960000      |
+| Algeria         | Africa     | 2381741    | 37100000     | 188681000     |
+| Andorra         | Europe     | 468        | 78115        | 3712000       |
+| Angola          | Africa     | 1246700    | 20609294     | 100990000     |
++-----------------+------------+------------+--------------+---------------+
+A country is big if it has an area of bigger than 3 million square km or a population of more than 25 million.
+
+Write a SQL solution to output big countries' name, population and area.
+
+For example, according to the above table, we should output:
+
++--------------+-------------+--------------+
+| name         | population  | area         |
++--------------+-------------+--------------+
+| Afghanistan  | 25500100    | 652230       |
+| Algeria      | 37100000    | 2381741      |
++--------------+-------------+--------------+
+您是否在真实的面试环节中遇到过这道题目呢？  
+题目难度：简单
+通过次数：5K
+提交次数：7.6K
+贡献者：LeetCode
+MySQL	
+
+1
+# Write your MySQL query statement below
+2
+select name,population,area from World where (area>3000000 or population>25000000)
