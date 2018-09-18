9e1e55e43b0c892e63be755cfa2e9eee55a651b5
1610353233@qq.com <1610353233@qq.com>
2018-09-18 22:05:04 +0800

    头晕

---
+++ src/main/java/com/learn/simple/L_563_Binary_Tree_Tilt.java
@@ -0,0 +1,37 @@
+package com.ricelink.utils;
+
+
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.*;
+
+public class test1 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode(int x) { val = x; }
+     * }
+     */
+/*    class Solution {
+        int result = 0;
+
+        public int findTilt(TreeNode root) {
+            helper(root);
+            return result;
+        }
+
+        public int helper(TreeNode root) {
+            if (root == null) return 0;
+            int leftSum = helper(root.left);
+            int rightSum = helper(root.right);
+            result += Math.abs(leftSum - rightSum);
+            return leftSum + rightSum + root.val;
+        }
+    }*/
+}
--- src/main/java/com/learn/simple/L_598_Range_Addition_II.java
+++ src/main/java/com/learn/simple/L_598_Range_Addition_II.java
@@ -1,30 +1,23 @@
-package com.ricelink.utils;
+package com.learn.simple;
 
+public class L_598_Range_Addition_II {
 
+	public static void main(String[] args) {
+		int m = 3;
+		int n = 3;
+		int[][] operations = { { 2, 2 }, { 3, 3 } };
+		System.out.println(maxCount(m, n, operations));
+	}
 
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.util.*;
-
-public class test1 {
-
-    public static void main(String[] args){
-       int m = 3;
-       int n = 3;
-       int[][] operations = {{2,2},{3,3}};
-       System.out.println(maxCount(m,n,operations));
-    }
-    public static int maxCount(int m, int n, int[][] ops) {
-        if (ops == null || ops.length == 0) {
-            return m * n;
-        }
-        int row = Integer.MAX_VALUE, col = Integer.MAX_VALUE;
-        for(int[] op : ops) {
-            row = Math.min(row, op[0]);
-            col = Math.min(col, op[1]);
-        }
-        return row * col;
-    }
+	public static int maxCount(int m, int n, int[][] ops) {
+		if (ops == null || ops.length == 0) {
+			return m * n;
+		}
+		int row = Integer.MAX_VALUE, col = Integer.MAX_VALUE;
+		for (int[] op : ops) {
+			row = Math.min(row, op[0]);
+			col = Math.min(col, op[1]);
+		}
+		return row * col;
+	}
 }
