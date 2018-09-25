24a09e64bc6322c59ef76c353d40960449ea0c53
1610353233@qq.com <1610353233@qq.com>
2018-09-25 22:13:37 +0800

    又一天

--- src/main/java/com/learn/simple/L_563_Binary_Tree_Tilt.java
+++ src/main/java/com/learn/simple/L_563_Binary_Tree_Tilt.java
@@ -1,37 +1,17 @@
-package com.ricelink.utils;
+package com.learn.simple;
 
-
-
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.util.*;
-
-public class test1 {
-    /**
-     * Definition for a binary tree node.
-     * public class TreeNode {
-     *     int val;
-     *     TreeNode left;
-     *     TreeNode right;
-     *     TreeNode(int x) { val = x; }
-     * }
-     */
-/*    class Solution {
-        int result = 0;
-
-        public int findTilt(TreeNode root) {
-            helper(root);
-            return result;
-        }
-
-        public int helper(TreeNode root) {
-            if (root == null) return 0;
-            int leftSum = helper(root.left);
-            int rightSum = helper(root.right);
-            result += Math.abs(leftSum - rightSum);
-            return leftSum + rightSum + root.val;
-        }
-    }*/
+public class L_563_Binary_Tree_Tilt {
+	/**
+	 * Definition for a binary tree node. public class TreeNode { int val;
+	 * TreeNode left; TreeNode right; TreeNode(int x) { val = x; } }
+	 */
+	/*
+	 * class Solution { int result = 0;
+	 * 
+	 * public int findTilt(TreeNode root) { helper(root); return result; }
+	 * 
+	 * public int helper(TreeNode root) { if (root == null) return 0; int
+	 * leftSum = helper(root.left); int rightSum = helper(root.right); result +=
+	 * Math.abs(leftSum - rightSum); return leftSum + rightSum + root.val; } }
+	 */
 }
--- src/main/java/com/learn/simple/L_645_Set_Mismatch.java
+++ src/main/java/com/learn/simple/L_645_Set_Mismatch.java
@@ -1,48 +1,31 @@
-package com.ricelink.utils;
-
-
-
-
-import com.alibaba.druid.sql.visitor.functions.Char;
-import com.ricelink.model.extractModel.SpAssessDetail;
-import com.ricelink.service.extractService.AssessExtract.AssessDetailExtracService;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.util.*;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+package com.learn.simple;
 
 public class L_645_Set_Mismatch {
 
-    public static  void main(String[] args){
-        // int[] nums = {1,2,2,4};
-         int[] nums = {3,2,2};
-        // int[] nums = {2,2};
-        int[] a=findErrorNums(nums);
-        for (int i=0;i< a.length;i++){
-            System.out.print(a[i]+",");
-        }
-    }
+	public static void main(String[] args) {
+		// int[] nums = {1,2,2,4};
+		int[] nums = { 3, 2, 2 };
+		// int[] nums = {2,2};
+		int[] a = findErrorNums(nums);
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + ",");
+		}
+	}
 
-    public static int[] findErrorNums(int[] nums) {
-        int[] res = new int[2];
-        for (int i : nums) {
-            if (nums[Math.abs(i) - 1] < 0){
-                res[0] = Math.abs(i);
-            } else{
-                nums[Math.abs(i) - 1] *= -1;
-            }
-        }
-        for (int i=0;i<nums.length;i++) {
-            if (nums[i] > 0){
-                res[1] = i+1;
-            }
-        }
-        return res;
-    }
+	public static int[] findErrorNums(int[] nums) {
+		int[] res = new int[2];
+		for (int i : nums) {
+			if (nums[Math.abs(i) - 1] < 0) {
+				res[0] = Math.abs(i);
+			} else {
+				nums[Math.abs(i) - 1] *= -1;
+			}
+		}
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] > 0) {
+				res[1] = i + 1;
+			}
+		}
+		return res;
+	}
 }
