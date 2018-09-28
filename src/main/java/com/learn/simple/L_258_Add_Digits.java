ea4d158d0399cfe27fbdd00cc82472db58c71868
1610353233@qq.com <1610353233@qq.com>
2018-09-28 21:12:33 +0800

    又学到了点

---
+++ src/main/java/com/learn/simple/L_258_Add_Digits.java
@@ -0,0 +1,12 @@
+package util;
+
+public class L_258_Add_Digits {
+
+    public static void main(String[] arg0){
+        int num=38;
+        System.out.println(addDigits(num));
+    }
+    public static int addDigits(int num) {
+        return num==0?0:(num%9==0?9:(num%9));
+    }
+}
