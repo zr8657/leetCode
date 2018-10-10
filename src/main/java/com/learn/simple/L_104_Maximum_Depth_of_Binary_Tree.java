bda345db9eb25705b0acc520e931da5e15a4dbb0
1610353233@qq.com <1610353233@qq.com>
2018-10-10 20:45:35 +0800

    又一天

---
+++ src/main/java/com/learn/simple/L_104_Maximum_Depth_of_Binary_Tree.java
@@ -0,0 +1,14 @@
+package com.learn.simple;
+
+public class L_104_Maximum_Depth_of_Binary_Tree {
+
+}
+// class Solution {
+// public int maxDepth(TreeNode root) {
+// if(root == null){
+// return 0;
+// }
+// int leftDepth = maxDepth(root.left) + 1;
+// int rightDepth = maxDepth(root.right) + 1;
+// return Math.max(leftDepth,rightDepth);
+// }
