f20adbbbea3a748a4148c502ceedccfef167238a
1610353233@qq.com <1610353233@qq.com>
2018-10-11 21:15:28 +0800

    又一天

---
+++ src/main/java/com/learn/simple/L_83_Remove_Duplicates_from_Sorted_List.java
@@ -0,0 +1,17 @@
+package org.webmaic.example;
+
+public class L_83_Remove_Duplicates_from_Sorted_List {
+//    class Solution {
+//        public ListNode deleteDuplicates(ListNode head) {
+//            if(head==null||head.next==null) return head;
+//            ListNode temp = head;
+//            while(temp.next != null){
+//                if(temp.val == temp.next.val){
+//                    temp.next = temp.next.next;
+//                }else{
+//                    temp=temp.next;
+//                }
+//            }
+//            return head;
+//        }
+}
