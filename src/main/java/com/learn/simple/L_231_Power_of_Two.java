b54767c9f4316e1a044dc7a7bdfaf141ead3b941
BIG..GGGGGG_WATERMELON <1610353233@qq.com>
2018-10-19 17:17:56 +0800

    111

---
+++ src/main/java/com/learn/simple/L_231_Power_of_Two.java
@@ -0,0 +1,14 @@
+package org.webmaic.example;
+
+public class L_231_Power_of_Two {
+
+    public static void main(String[] arg0){
+
+        System.out.println(isPowerOfTwo(1073741825));
+    }
+
+    public static boolean isPowerOfTwo(int n) {
+        return n > 0 && (1073741824 % n == 0);
+    }
+
+}
