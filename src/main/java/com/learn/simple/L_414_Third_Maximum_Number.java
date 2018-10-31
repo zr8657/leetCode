31885ea2ab02a939647237ac8d3641cca178ace7
BIG..GGGGGG_WATERMELON <1610353233@qq.com>
2018-10-31 18:28:36 +0800

    又一天

---
+++ src/main/java/com/learn/simple/L_414_Third_Maximum_Number.java
@@ -0,0 +1,37 @@
+package org.webmaic.example;
+
+import org.hibernate.validator.constraints.SafeHtml;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.webmaic.example.util.WebdriverUtil;
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+public class L_414_Third_Maximum_Number {
+    public static void main(String[] arg0) {
+        int nums[] = {2, 2, 3, 1};
+        System.out.println(thirdMax(nums));
+    }
+
+    public static int thirdMax(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            if (!set.contains(i)) {
+                pq.offer(i);
+                set.add(i);
+                if (pq.size() > 3) {
+                    set.remove(pq.poll());
+                }
+            }
+        }
+        if (pq.size() < 3) {
+            while (pq.size() > 1) {
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+}
