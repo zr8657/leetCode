87788fe704281aa9301ae18c5779f1902d350b46
BIG..GGGGGG_WATERMELON <1610353233@qq.com>
2018-11-29 18:18:28 +0800

    -----

---
+++ src/main/java/com/learn/simple/L_027_Remove_Element.java
@@ -0,0 +1,23 @@
+package org.webmaic.example;
+
+import org.hibernate.validator.constraints.SafeHtml;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.webmaic.example.util.WebdriverUtil;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class L_27_Remove_Element {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+
+}
