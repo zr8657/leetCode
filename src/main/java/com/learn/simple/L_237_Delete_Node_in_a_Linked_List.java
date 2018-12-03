66ba573f372dc767171dabb49d80bde4ec060ff0
BIG..GGGGGG_WATERMELON <1610353233@qq.com>
2018-12-03 19:48:13 +0800

    ???

---
+++ src/main/java/com/learn/simple/L_237_Delete_Node_in_a_Linked_List.java
@@ -0,0 +1,17 @@
+package org.webmaic.example;
+
+import org.hibernate.validator.constraints.SafeHtml;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.webmaic.example.util.WebdriverUtil;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class L_237_Delete_Node_in_a_Linked_List {
+//    public void deleteNode(ListNode node) {
+//        node.val=node.next.val;
+//        node.next=node.next.next;
+//    }
+
+}
