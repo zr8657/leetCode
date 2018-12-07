25a8a664e17a518d98e9fba5a600255bdf45e4e0
BIG..GGGGGG_WATERMELON <1610353233@qq.com>
2018-12-07 18:02:49 +0800

    ---

---
+++ src/main/java/com/learn/simple/L_590_N_ary_Tree_Postorder_Traversal.java
@@ -0,0 +1,21 @@
+package com.ricelink.beans;
+
+public class L_590_N_ary_Tree_Postorder_Traversal {
+//    class Solution {
+//        public List<Integer> postorder(Node root) {
+//            List<Integer> list = new ArrayList<>();
+//            return handler(root, list);
+//        }
+//        public List<Integer> handler(Node root, List<Integer> list){
+//            if (root == null){
+//                return list;
+//            }
+//            for(Node node: root.children){
+//                handler(node, list);
+//            }
+//            list.add(root.val);
+//            return list;
+//
+//
+//        }
+}
